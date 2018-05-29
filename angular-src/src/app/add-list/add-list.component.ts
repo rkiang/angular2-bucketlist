50bbfe25faebbe677c36f69677eb35268e548710
rkiang <ryanlkiang@gmail.com>
2018-05-29 11:13:34 -0500

    added to view and add list html and connected their ts files too

--- angular-src/src/app/add-list/add-list.component.ts
+++ angular-src/src/app/add-list/add-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { List } from '../models/List';
+import { ListService } from '../services/list.service';
 
 @Component({
   selector: 'app-add-list',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-list.component.css']
 })
 export class AddListComponent implements OnInit {
-
-  constructor() { }
+  private newList: List;
+  @Output() addList: EventEmitter<List> = new EventEmitter<List>();
+  constructor(private listServ: ListService) { }
 
   ngOnInit() {
+    this.newList = {
+      title: '',
+      category: '',
+      description: '',
+      _id: ''
+
+    }
   }
 
+  public onSubmit() {
+    console.log(this.newList.category);
+    this.listServ.addList(this.newList).subscribe(
+      response => {
+
+        if (response.success == true)
+          this.addList.emit(this.newList);
+      },
+    );
+  }
 }
--- angular-src/src/app/view-list/view-list.component.ts
+++ angular-src/src/app/view-list/view-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService } from '../services/list.service';
+import { List } from '../models/List'
 
 @Component({
   selector: 'app-view-list',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewListComponent implements OnInit {
 
-  constructor() { }
+  //lists propoerty which is an array of List type
+  private lists: List[] = [];
+
+  constructor(private listServ: ListService) { }
 
   ngOnInit() {
+
+    //Load all list on init
+    this.loadLists();
   }
 
+  public loadLists() {
+
+    //Get all lists from server and update the lists property
+    this.listServ.getAllLists().subscribe(
+      response => this.lists = response, )
+  }
+
+  //deleteList. The deleted list is being filtered out using the .filter method
+  public deleteList(list: List) {
+    this.listServ.deleteList(list._id).subscribe(
+      response => this.lists = this.lists.filter(lists => lists !== list), )
+  }
+
+  public onAddList(newList) {
+    this.lists = this.lists.concat(newList);
+  }
 }
